06c0e00a5bafc3a8e0b121ab0bcff831e7844bd3
Jeffrey <jpichardo163@gmail.com>
2017-05-22 17:11:09 -0400

    working on findAll error

---
+++ db/seeds/seed.sql
@@ -0,0 +1,10 @@
+\c yilp_dev
+
+INSERT INTO users (name, email, password) VALUES 
+('John Doe','JohnDoe@gmail.com','***********');
+
+INSERT INTO location (location) VALUES 
+('Bronx');
+
+INSERT INTO reviews (review, location, user_id) VALUES 
+('It Stinks and it just sucks',1,1);
