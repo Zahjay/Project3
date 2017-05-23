65bb2b703a31b26e6eaceb48e4b6edef4e91e165
DTaitt <donovan.taitt@yahoo.com>
2017-05-23 16:39:43 -0400

    added index components

---
+++ client/src/components/Index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+//import components
+import Header from './Header.jsx';
+import Nav from './Nav.jsx';
+import Auth from './Auth.jsx';
+
+class Index extends Component {
+	render() {
+		return(
+			<div>
+				<Header />
+				<Nav />
+				<Auth />
+			</div>
+		)
+	}
+}
+
+export default Index;
