43c1693f24ebd6064ef6aba6702fe6e0863ec0d1
zahra abdallah <zkon360@gmail.com>
2017-05-24 10:56:29 -0400

    Added to file structue of components

--- client/src/components/Boroughpage.jsx
+++ client/src/components/Boroughpage.jsx
@@ -6,6 +6,10 @@ import Filterstores  from './Filterstores.jsx';
 
 import Headborough from './Headborough.jsx';
 
+import Dropdownfilter from './Dropdownfilter.jsx';
+
+import Storelist from './Storelist.jsx';
+
 import Toogle from './Toogle.jsx';
 
 class Boroughpage extends Component{
@@ -14,38 +18,11 @@ class Boroughpage extends Component{
     return(
         <div>
             <Headborough/>
-            <img src="placeholder.png" alt="Borough Pics" />
+            <img src="placeholder.png" alt="Main Pic" />
             <Filterstores />
+            <Dropdownfilter/>
             <main>
-            <form onSubmit={this.handleSubmit}>
-          <label>
-            Sorted by: Top Rated
-            <select>
-              <option value="Restuarant">Restuarant</option>
-              <option value="Retail">Retail</option>
-              <option selected value="Nightlife">Nightlife</option>
-            </select>
-          </label>
-        </form>
-        <div className="Boroughpage-content-rendered"> 
-         <h3> Restaurant Name</h3>
-         <h2>100 BroadwayStreet, New York, NY</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-        </div>
-
-         <div> 
-         <h3> Restaurant Name</h3>
-         <h2>100 BroadwayStreet, New York, NY</h2>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        </div>
-
-         <div> 
-         <h3> Restaurant Name</h3>
-         <h2>100 BroadwayStreet, New York, NY</h2>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-        </div>
-
-
+            <Storelist/>
         </main>
       </div>
       
--- client/src/components/Headborough.jsx
+++ client/src/components/Headborough.jsx
@@ -10,7 +10,6 @@ class Headborough extends Component {
       
       <div className="Borough-userinfo"> 
         <Image src="/assets/thumbnail.png" circle />
-
         <p className="username">Username</p>
         <div> 
         <img src="placeholder.png" alt="Our logo" className="Borough-logo" /> 
