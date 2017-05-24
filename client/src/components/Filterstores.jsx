b4a1b0fa0e1901e637f59932e6fd10f3d21cd548
zahra abdallah <zkon360@gmail.com>
2017-05-23 21:33:31 -0400

    Fixed tabs nav in Filterstore.jsx, added user pic,etc

---
+++ client/src/components/Filterstores.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import 
+{Tabs,
+  Tab} from 'react-bootstrap';
+
+class Filterstores extends Component{
+  render(){
+   return(
+      <div>
+          <div className="Navthreeoptions">
+            <nav>
+               <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+                <Tab eventKey={1} title="Food"> Restaurant Content</Tab>
+                <Tab eventKey={2} title="Night"> Night Life</Tab>
+                <Tab eventKey={3} title="Retail">Shopping Locations</Tab>
+              </Tabs>
+            </nav>
+          </div>
+        </div>
+      
+   );
+  }
+}
+
+export default Filterstores;
---
+++ client/src/components/Headborough.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Headborough extends Component {
+  render(){
+    return(
+      
+      <div className="Borough-userinfo"> 
+        <img src="placeholder.png" alt="user pic" className="userpic" />
+        <p className="username">Username</p>
+        <div> 
+        <img src="placeholder.png" alt="Our logo" className="Borough-logo" /> 
+      </div>
+    </div>
+    );
+  }
+}
+
+export default Headborough;
+
+
+
