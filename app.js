d14ba5914f2fbec6f9798c01c2b6de486029854e
Jeffrey <jpichardo163@gmail.com>
2017-05-25 17:37:25 -0400

    it told me to commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const cookiParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
@@ -22,7 +22,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use(logger('dev'));
-app.use(cookiParser());
+app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
